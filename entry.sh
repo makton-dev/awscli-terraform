dfac3df9712094a62e283460bf868cfd4bdfda15
Michael Schneider <mschneider@sedev.local>
2022-03-27 08:18:45 -0700

    little update for entry script

--- entry.sh
+++ entry.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 set -eo pipefail
 
-# Set Terraform Variable via Environment
-ENV="$Something"
-
 # Enable console. this is done after ENVs as they are sensitive
 set -x
 
@@ -13,7 +10,7 @@ terraform init
 # Validate Terraform plan
 terraform plan -out aws.tfplan
 
-# Execute Terraform plan
-if [ "${apply}" == "true" ]; then
-  terraform apply aws.tfplan
-fi
+# Execute Terraform apply
+# if [ "${apply}" == "true" ]; then
+#   terraform apply aws.tfplan
+# fi
